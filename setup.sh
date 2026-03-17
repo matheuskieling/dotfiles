8c077c67cbd7c4e99b7c7b6c91d39f5660bf0521
Matheus Kieling <matheus.kieling@hotmail.com>
2026-03-17 13:21:00 -0300

    wip

--- setup.sh
+++ setup.sh
@@ -12,13 +12,29 @@ git config --global credential.useHttpPath true
 
 # ---- Clone Dotfiles ----
 cd ~
-git clone https://github.com/matheuskieling/dotfiles.git
+if [ -d ".git" ]; then
+  echo "Já dentro do repo"
+else
+  cd ~
+  if [ ! -d "dotfiles" ]; then
+    git clone https://github.com/matheuskieling/dotfiles.git
+  fi
+  cd dotfiles
+fi
 
 # ---- Configuração Keyd via Stow ----
 cd ~/dotfiles
-sudo stow keyd -t /
+sudo stow keyd -t / --adopt
+stow xkb --adopt
 sudo stow nvim --adopt
 sudo stow alacritty --adopt
 sudo stow hypr --adopt
 sudo stow waybar --adopt
 cd ~
+
+# ---- CLAUDE CODE ----
+if ! command -v claude >/dev/null 2>&1; then
+  curl -fsSL https://claude.ai/install.sh | bash
+fi
+
+sudo keyd reload
