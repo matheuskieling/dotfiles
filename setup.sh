3e488ede46cbea099f161e6099023f9a48a23472
Matheus Kieling <matheus.kieling@hotmail.com>
2026-03-17 11:31:44 -0300

    wip

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
-sudo pacman -S stow --noconfirm ---needed
-sudo pacman -S keyd --noconfirm --needed
+#!/bin/bash
+set -e  # sai se algum comando falhar
 
+# ---- Pacotes ----
+sudo pacman -S --noconfirm --needed stow
+sudo pacman -S --noconfirm --needed keyd
+sudo pacman -S --noconfirm --needed libsecret
 
-cd ~/dotfiles && sudo stow keyd -t /
+# ---- Configurações Git ----
+git config --global credential.helper libsecret
+git config --global credential.useHttpPath true
+
+# ---- Configuração Keyd via Stow ----
+cd ~/dotfiles
+sudo stow keyd -t /
+sudo stow nvim --adopt
+sudo stow alacritty --adopt
+sudo stow hypr --adopt
 cd ~
