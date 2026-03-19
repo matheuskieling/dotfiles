d00fc4eef27a271c6ead7bfb2d026d0c22bdc1fc
Matheus Kieling <matheus.kieling@hotmail.com>
2026-03-19 14:28:24 -0300

    update dotfiles

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Center cursor after vertical movements
+vim.keymap.set("n", "j", "gjzz", { desc = "Move down (visual line) and center" })
+vim.keymap.set("n", "k", "gkzz", { desc = "Move up (visual line) and center" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half page and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half page and center" })
